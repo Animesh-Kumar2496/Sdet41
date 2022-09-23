c33088a4189c045af465426d6798e811075d6f4e
ANIMESH KUMAR <72871395+Animesh-Kumar2496@users.noreply.github.com>
2022-09-23 11:37:04 +0530

    1st commit

---
+++ src/main/java/Practice1.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+
+public class Practice1 {
+	@Test
+	public Practice1(){
+		System.out.println("Practice1");
+		
+	}
+
+}
---
+++ src/main/java/Practice2.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+
+public class Practice2 {
+	@Test
+	public Practice2() {
+		System.out.println("practice2");
+		
+	}
+
+}
